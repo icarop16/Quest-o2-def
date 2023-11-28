1f9ed6c16ce2bff492313a8877069a4cd9965744
icarop16 <147431562+icarop16@users.noreply.github.com>
2023-11-28 16:48:54 -0300

    Add files via upload

---
+++ pedro.py
@@ -0,0 +1,18 @@
+def soma (N1, N2):
+    soma = N1 + N2 
+    print(soma)
+def subtração (N1, N2):
+    subtração = N1 - N2 
+    print(subtração)
+def multiplicação (N1, N2):
+    multiplicação = N1 * N2 
+    print(multiplicação)
+def divisão (N1, N2):
+    divião = N1 / N2 
+    print(divião)
+N1 = int(input("Digite um número: "))
+N2 = int(input("Digite um número: "))
+soma (N1,N2)
+subtração (N1,N2)
+multiplicação (N1,N2)
+divisão (N1,N2)
